87c54c7de2ae6e85afd1f93daf1db9387163fdc1
pratik vishwakarma <pratikvishwakarma748@gmail.com>
2026-09-18 03:00:06 +0530

    commit

--- Backend/socket/socket.js
+++ Backend/socket/socket.js
@@ -6,7 +6,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "https://real-time-chat-application-1-gd0r.onrender.com",
+        origin: "http://localhost:5173",
         credentials: true,
     }
 });
