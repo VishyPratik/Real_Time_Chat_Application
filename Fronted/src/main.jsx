f9dee2086218cd4e3f1daee0a896c4655a71b680
pratik vishwakarma <pratikvishwakarma748@gmail.com>
2026-09-17 08:42:19 +0530

    last commit

--- Fronted/src/main.jsx
+++ Fronted/src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router'
 import { store } from './redux/Store.jsx'
 import {Provider} from "react-redux"
 
-export const serverUrl ="https://real-time-chat-application-1-gd0r.onrender.com/"
+export const serverUrl ="https://real-time-chat-application-1-gd0r.onrender.com"
 createRoot(document.getElementById('root')).render(
     <BrowserRouter>
         <Provider store={store}>
