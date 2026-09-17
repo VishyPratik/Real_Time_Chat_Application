ef21dff17b2fae58eb6ca4cfcf28bf9d906f78ec
pratik vishwakarma <pratikvishwakarma748@gmail.com>
2026-09-18 03:42:14 +0530

    commit

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,7 @@ const messageRouter=require("./Routes/messageRoute");
 const { app, server } = require("./socket/socket");
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://real-time-chat-application-fronted.onrender.com",
     credentials: true,
 
 }))
